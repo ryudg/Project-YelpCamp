e7194b989e2503d5f3d944bce8be1b38a8ff6715
ryudg <ryudg95@gmail.com>
2023-02-01 00:42:37 +0900

    CREATE
    CREATE

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 // home
 app.get("/", (req, res) => {
@@ -33,7 +34,25 @@ app.get("/", (req, res) => {
 // campgrounds
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
-  res.render("campgrounds/index");
+  res.render("campgrounds/index", { campgrounds });
+});
+
+// CREATE
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+// CREATE POST
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+// CREATE 라우트가 :id 라우트보다 밑에 있으면 new경로를 id처리하기 떄문에 위에 있어야함
+
+// campgrounds:id
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campgrounds/show", { campground });
 });
 
 app.listen(3000, () => {
