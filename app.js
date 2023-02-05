9f9046e6a3e043de69be50d7ecc663a97efdbc2b
ryudg <ryudg95@gmail.com>
2023-02-05 18:14:52 +0900

    Create Write Reviews Route
    Create Write Reviews Route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { campgroundSchema } = require("./schema");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const Campground = require("./models/campground");
+const Review = require("./models/review");
 
 mongoose.set("strictQuery", true);
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
@@ -143,6 +144,19 @@ app.delete(
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect("/campgrounds");
+  })
+);
+
+// Review
+app.post(
+  "/campgrounds/:id/reviews",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
