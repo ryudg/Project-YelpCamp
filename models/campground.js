1257d6a01eb8d0aa042391510f910b686e9b8bd3
ryudg <ryudg95@gmail.com>
2023-02-05 19:22:33 +0900

    Delete Review in Campground
    Delete Review in Campground
    리뷰가 있는 캠핑장을 삭제하면
    리뷰 데이터는 남아있게 되는데
    캠핑장을 삭제할 때 리뷰도 함께 삭제하기

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 const Schema = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
@@ -13,6 +14,18 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+
+// findByIdAndDelete가 작동시키는 미들웨어는 findOneAndDelete
+// findByIdAndDelete를 사용하면서 remove나 removeOne같은 미들웨어를 설정하면 작동하지 않음
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.deleteMany({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
