db10d14978e79956be58e3af6e395a038ef3a6ab
ryudg <ryudg95@gmail.com>
2023-02-09 22:09:12 +0900

    register error handling
    catchAsync을 이용한 가입 에러 처리

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,27 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
 
 router.get("/register", (req, res) => {
   res.render("users/register");
 });
-router.post("/register", async (req, res) => {
-  res.send(req.body);
-});
+// 새로운 사용자 생성
+// 사용자 모델 인스턴스 생성
+router.post(
+  "/register",
+  catchAsync(async (req, res) => {
+    try {
+      const { email, username, password } = req.body;
+      const user = new User({ email, username });
+      const registerUser = await User.register(user, password);
+      req.flash("success", "Welcome to Yelp Camp");
+      res.redirect("/campgrounds");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("register");
+    }
+  })
+);
 
 module.exports = router;
