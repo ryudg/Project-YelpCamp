0ba57336273bc838dd05d9b60a428c372f2061ab
ryudg <ryudg95@gmail.com>
2023-02-11 01:41:22 +0900

    router.route("경로")
    router.route("경로") 리팩터링

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -9,10 +9,17 @@ const catchAsync = require("../utils/catchAsync");
 const Campground = require("../models/campground");
 const { isLoggedIn, validateCampground, isAuthor } = require("../middleware");
 
-// campgrounds
-router.get("/", catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index)) // campgrounds
+  .post(
+    // CREATE 라우트가 :id 라우트보다 밑에 있으면 new경로를 id처리하기 떄문에 위에 있어야함
+    isLoggedIn,
+    validateCampground, // 라우터 핸들러에 Joi 미들웨어 추가 // mongoose에서 이곳으로 발생된 오류가 있다면 캐스트 오류를 통해 확인하고
+    catchAsync(campgrounds.createCampground) // 더이상 try...catch를 할 필요가 없이 catchAsync함수를 사용 , 오류가 발생하면 catchAsync가 검출하고 next로 전달한다.
+  );
 
-// CREATE
+// CREATE - /:id 앞에 와야함
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 // CREATE POST
 // app.post("/campgrounds", async (req, res, next) => {
@@ -26,19 +33,17 @@ router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 //   }
 // });
 
-// CREATE 라우트가 :id 라우트보다 밑에 있으면 new경로를 id처리하기 떄문에 위에 있어야함
-// 더이상 try...catch를 할 필요가 없이 catchAsync함수를 사용
-router.post(
-  "/",
-  isLoggedIn,
-  validateCampground, // 라우터 핸들러에 Joi 미들웨어 추가
-  catchAsync(campgrounds.createCampground)
-);
-// mongoose에서 이곳으로 발생된 오류가 있다면 캐스트 오류를 통해 확인하고
-// 오류가 발생하면 catchAsync가 검출하고 next로 전달한다.
-
-// READ campgrounds:id
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground)) // READ campgrounds:id
+  .put(
+    // UPDATE PUT
+    isLoggedIn,
+    isAuthor,
+    validateCampground, // 라우터 핸들러에 Joi 미들웨어 추가
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground)); // DELETE
 
 // UPDATE
 router.get(
@@ -46,22 +51,6 @@ router.get(
   isLoggedIn,
   isAuthor,
   catchAsync(campgrounds.renderEditForm)
-);
-// UPDATE PUT
-router.put(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  validateCampground, // 라우터 핸들러에 Joi 미들웨어 추가
-  catchAsync(campgrounds.updateCampground)
-);
-
-// DELETE
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isAuthor,
-  catchAsync(campgrounds.deleteCampground)
 );
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -5,27 +5,27 @@ const User = require("../models/user");
 const users = require("../controllers/users");
 const catchAsync = require("../utils/catchAsync");
 
-router.get("/register", users.renderRegister);
-// 새로운 사용자 생성
-// 사용자 모델 인스턴스 생성
-router.post("/register", catchAsync(users.register));
+router
+  .route("/register")
+  .get(users.renderRegister)
+  .post(catchAsync(users.register)); // 새로운 사용자 생성(사용자 모델 인스턴스 생성)
 
-// login
-router.get("/login", users.renderLogin);
-// 로그인 전략, local(facebook,google 여러개 설정 가능)
-// passport.authenticate 두번째 인자의 객체에 지정할 수 있는 옵션
-// failureFlash(플래시 메시지 띄우기), failureRedirect(login 실패시 리다이렉트)
-router.post(
-  "/login",
-  passport.authenticate("local", {
-    // authenticate는 미들웨어 역할을 하는데 사용자 계정을 등록하기 전까지는 인증할게 없다.
-    // 그래서 passport의 req.login을 사용함
-    failureFlash: true,
-    failureRedirect: "/login",
-    keepSessionInfo: true,
-  }),
-  users.login
-);
+router
+  .route("/login")
+  .get(users.renderLogin) // login
+  // 로그인 전략, local(facebook,google 여러개 설정 가능)
+  // passport.authenticate 두번째 인자의 객체에 지정할 수 있는 옵션
+  // failureFlash(플래시 메시지 띄우기), failureRedirect(login 실패시 리다이렉트)
+  .post(
+    passport.authenticate("local", {
+      // authenticate는 미들웨어 역할을 하는데 사용자 계정을 등록하기 전까지는 인증할게 없다.
+      // 그래서 passport의 req.login을 사용함
+      failureFlash: true,
+      failureRedirect: "/login",
+      keepSessionInfo: true,
+    }),
+    users.login
+  );
 
 // logout
 router.get("/logout", users.logout);
