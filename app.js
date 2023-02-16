47d1ec3f144e210509df246c1f944b1fa91f6ca1
Ryu <ryudg95@gmail.com>
2023-02-16 20:42:27 +0900

    Basic Security use to Helmet

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const passport = require("passport");
 const LocalStarategy = require("passport-local");
 const User = require("./models/user");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 // 라우터 불러오기
 const userRoutes = require("./routes/users");
@@ -62,8 +63,78 @@ app.use(passport.session()); // 영구 로그인 세션, session()은 passport.s
 passport.use(new LocalStarategy(User.authenticate())); // passport가 LocalStarategy를 사용, LocalStarategy에 대해 인증 메서드는 사용자 모델에 위치하게됨(authenticate)
 passport.serializeUser(User.serializeUser()); // passport에게 사용자를 어떻게 직렬화 하는지 알려줌, 직렬화는 어떻게 데이터를 얻고 세선세어 사용자를 저장하는지를 참조함
 passport.deserializeUser(User.deserializeUser()); // 즉, 세선에 정보를 어떻게 저장하고 가져오는지를 결정하는 메서드이다.
-
-app.use(mongoSanitize());
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
+const scriptSrcUrls = [
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://api.mapbox.com/",
+  "https://kit.fontawesome.com/",
+  "https://cdnjs.cloudflare.com/",
+  "https://cdn.jsdelivr.net",
+];
+const styleSrcUrls = [
+  "https://kit-free.fontawesome.com/",
+  "https://stackpath.bootstrapcdn.com/",
+  "https://api.mapbox.com/",
+  "https://api.tiles.mapbox.com/",
+  "https://fonts.googleapis.com/",
+  "https://use.fontawesome.com/",
+  "https://cdn.jsdelivr.net",
+];
+const connectSrcUrls = [
+  "https://api.mapbox.com/",
+  "https://a.tiles.mapbox.com/",
+  "https://b.tiles.mapbox.com/",
+  "https://events.mapbox.com/",
+];
+const fontSrcUrls = [];
+app.use(
+  helmet({
+    // Cross-Origin Embedder Policy (COEP)를 활성화하는 설정. 이를 통해, 애플리케이션이 다른 도메인에서 로드된 리소스를 제어할 수 있습니다. false로 설정하면 COEP를 비활성화.
+    // COEP는 웹 애플리케이션 보안을 강화하기 위한 보안 메커니즘 중 하나.
+    // 이 정책은 iframe, object, embed 요소의 크로스 오리진 로딩을 제한하고 이러한 요소의 리소스가 동일한 출처에서 로드되도록 강제
+    // COEP는 iframe 및 브라우저의 자체 보안 기능을 강화하여 다른 도메인에서 호스팅되는 페이지로부터의 공격을 방지.
+    // crossOriginEmbedderPolicy: false는 브라우저가 Cross-Origin Embedder Policy를 적용하지 않도록 하며, 사용자가 웹 사이트를 더 쉽게 사용할 수 있도록 도와주지만, 보안을 감소시킬 수 있으므로 이 값을 변경하기 전에 신중히 검토해야함
+    crossOriginEmbedderPolicy: false,
+    // Cross-Origin Resource Policy (CORP)를 활성화하는 설정. 이를 통해, 애플리케이션이 다른 도메인에서 로드된 리소스를 허용할지 제어할 수 있습니다. 아래 코드에서는 모든 도메인("*")에서 리소스를 허용하도록 설정하고 있다.
+    // 웹 페이지에서 리소스의 로딩 및 배포에 대한 제어를 제공. 이것은 다른 출처에서 제공되는 리소스의 사용을 제한하고, 신뢰할 수 있는 출처로부터 제공되는 리소스를 사용하도록 촉진
+    // allowOrigins 속성을 사용하여 리소스를 제공할 수 있는 출처를 설정할 수 있으며, 리소스를 사용할 수 있는 도메인의 목록을 지정할 수 있다.
+    crossOriginResourcePolicy: {
+      allowOrigins: ["*"],
+    },
+    // `contentSecurityPolicy: false` 해당 애플리케이션에서 Content Security Policy (CSP)를 사용하지 않겠다는 것을 의미
+    // CSP는 웹 애플리케이션 보안을 강화하는 기술 중 하나로, 웹 애플리케이션에서 로드되는 리소스들의 출처를 제한하거나, 허용되는 실행 방식을 제한하여 XSS, 데이터 탈취 등의 공격을 방지할 수 있다.
+    // 따라서, 대부분의 경우에는 CSP를 활성화하는 것이 좋은 보안적인 선택
+    // contentSecurityPolicy: false로 설정하는 것은 해당 애플리케이션에서 CSP를 사용하지 않겠다는 것이므로, CSP 관련 설정을 하지 않아도 됨.
+    // 이 경우, CSP에 관련된 오류나 경고 메시지를 피할 수 있지만, 보안적인 취약점이 발생할 가능성이 높아지므로 주의
+    // CSP는 애플리케이션에서 로드되는 리소스들의 출처를 제한하여, XSS와 같은 보안 공격을 방지하는 역할.
+    // 아래 코드에서는 directives를 사용하여, defaultSrc, connectSrc, scriptSrc, styleSrc, workerSrc, objectSrc, imgSrc, fontSrc 등의 CSP 설정을 추가하고 있다.
+    // 각각의 Src 디렉티브는 허용할 출처를 지정한다. 여기서 self는 애플리케이션의 현재 도메인을 의미한다. unsafe-inline, data:, blob:, 특정 도메인 등의 값을 추가하여, 허용할 리소스 출처를 지정할 수 있다.
+    // 아래 코드에서는 Cloudinary 이미지 리소스와 Unsplash 이미지 리소스를 허용하도록 설정하고 있다.
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [],
+        connectSrc: ["'self'", ...connectSrcUrls],
+        scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+        styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+        workerSrc: ["'self'", "blob:"],
+        objectSrc: [],
+        imgSrc: [
+          "'self'",
+          "blob:",
+          "data:",
+          "https://res.cloudinary.com/dcibff1dd/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+          "https://images.unsplash.com/",
+        ],
+        fontSrc: ["'self'", ...fontSrcUrls],
+      },
+    },
+  })
+);
 
 app.use((req, res, next) => {
   // console.log(req.session);
